8d3a6d496966a921e9e3161f4e59ac8498fe60d9
ricardo140 <ricardoleon878@gmail.com>
2023-11-16 16:50:47 +0100

    different devices

---
+++ device1_simulation.py
@@ -0,0 +1,27 @@
+from classes.device import Device
+import multiprocessing
+
+logging_port = 30303
+
+devices = [
+    (9001, "device_1")
+]
+device_list = []
+def create_listner(port, device_id):
+    device = Device("localhost", port, device_id, logging_port)
+    device_list.append(device)
+    print(f"Created {device_id} on port {port}")
+    try:
+        device.listen()
+    except KeyboardInterrupt:
+        print("We have finished this method")
+        device.close()
+
+
+
+if __name__ == "__main__":
+
+    for port, name in devices:
+        device_process = multiprocessing.Process(target=create_listner, args=(port, name))
+        device_process.start()
+    
